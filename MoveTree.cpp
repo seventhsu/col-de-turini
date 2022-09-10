dd137791b5cb1868d4b83aaafae3e1de2f971661
Ali <alialbaghdadi6@gmail.com>
2022-09-10 01:14:54 -0500

    Creating base files

---
+++ MoveTree.cpp
@@ -0,0 +1,6 @@
+#include "MoveTree.hpp"
+
+// The pyramid is not a square 2D matrix, it is a pyramid: the row lists are shorter at the top and get longer as they go down.
+MoveTree::MoveTree(int[][] pyramid, std::vector deck) {
+	
+}
---
+++ MoveTree.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+class MoveTree {
+	public:
+		MoveTree(int[][] pyramid, int[] deck);
+	private:
+		MoveNode *movetree;
+};
+
+struct MoveNode {
+	// how to denote moves??
+};
