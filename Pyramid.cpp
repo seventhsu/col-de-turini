84475910b447deab939e1d51c6e0d32db2876a6a
seventhsu <alialbaghdadi6@gmail.com>
2022-09-11 01:15:46 -0500

    Stock is now stored in a vector. Accounts for matching two adjacent cards from the stock.

--- Pyramid.cpp
+++ Pyramid.cpp
@@ -8,10 +8,6 @@ Pyramid::Pyramid(std::string fileloc) {
     // Init array of row arrays
     pyramidArr = (int**) malloc(7 * sizeof(int*));
 
-    // Initialize the stock as full. 13 types of cards, 4 cards each
-    stock = new std::map<int, int>;
-    for (int val = 1; val <= 13; val++) stock->insert(std::make_pair(val, 4));
-
     // Loop over rows
     for (int r = 0; r < 7; r++) {
         // Malloc space for each pyramid row, just in time
@@ -35,11 +31,45 @@ Pyramid::Pyramid(std::string fileloc) {
             else if (token == "K") cardValue = 13;
             else cardValue = stoi(token);
 
-            // Take this card out of the stock
-            (*stock)[cardValue] -= 1;
-
             // Put this card on the pyramid (array)
             pyramidArr[r][pos] = cardValue;
         }
     }
+
+    // Initialize the stock
+    stock = new std::vector<int>;
+    std::string stockstr;
+    std::getline(fs, stockstr);
+    std::istringstream iss(stockstr);
+    for (int pos = 0; pos < 24; pos++) { // In a 7-high pyramid of 52 cards, the stock must be 24 cards long
+        // Grab the first space-delimited chunk and store it in token
+        std::string token;
+        std::getline(iss, token, ' ');
+
+        // Attempt to interpret it as a value
+        int cardValue = -1;
+        if (token == "A") cardValue = 1;
+        else if (token == "J") cardValue = 11;
+        else if (token == "Q") cardValue = 12;
+        else if (token == "K") cardValue = 13;
+        else cardValue = stoi(token);
+
+        // Put this card on the stack
+        stock->push_back(cardValue);
+    }
+}
+
+Pyramid::~Pyramid() {
+    for (int i = 0; i < 7; i++) delete pyramidArr[i];
+    delete pyramidArr;
+    delete stock;
+}
+
+// We pass the list of moves made so far to construct the state of the game at each recursion
+MatchNode* Pyramid::constructMatchTree(std::vector<CardPair> moveList) {
+    // Reconstruct game state (i.e. touchables and stock) from move list
+    // Enumerate all valid combinations of P-P, P-S, S-S (P = pyramid, S = stock)
+    // Init each one as a node in this vector's nextlist
+    // Loop over nextlist again and recurse for each one (breadth-first strat)
+    return NULL;
 }
--- Pyramid.hpp
+++ Pyramid.hpp
@@ -11,30 +11,34 @@
  * Can use this to enumerate the locations of important cards for use later
  */
 typedef struct PyramidCardStruct {
-	int r;
-	int pos;
+	int row; // Ranges from 0 to 6, -1 if card represented is in stock
+	int pos; // If card is in stock, ranges from 0 to 23, else ranges from 0 to 6
 } Card;
 
 /* An m-ary tree that enumerates all possible moves in the game.
  * It holds information on which two cards should be matched,
  * and where the game can go from there (the child nodes).
  */
-typedef struct MatchTreeNodeStruct {
-	bool both_pyramid; // = true if matching two pyramid cards, = false if matching a pyramid card and a stock card
+
+typedef struct MatchingPairStruct {
+	// If card is from stock, row = -1 and pos will be position from top of stock
 	Card card1;
-	Card card2; // defined if both_pyramid == true, (-1, -1) otherwise
-	// No need to store stock card if we are matching from pyramid and stock. We know card1's value + stock card = 13
-	
-	std::vector<MatchTreeNodeStruct*> nextMatches; // The next possible "game states"
+	Card card2;
+} CardPair;
+
+typedef struct MatchTreeNodeStruct {
+	CardPair pair;
+	std::vector<MatchTreeNodeStruct*> *next; // The next possible "game states"
 } MatchNode;
 
 class Pyramid {
 	public:
 		Pyramid(std::string fileloc);
+		~Pyramid();
+		MatchNode* constructMatchTree(std::vector<CardPair> moveList);
 	private:
 		int** pyramidArr; // A pyramidal "2-D" array. pyramidArr[0] has 1 element, pyramidArr[1] has 2, etc.
-		std::map<int, int> *stock; // arg0 is card face value (1-13), arg1 is how many in stock
+		std::vector<int> *stock; // The stock is ordered: we cannot just match any two cards inside it
 
-		std::vector<Card> touchables; // Locations of all pyramid cards which are not covered off by other cards
-		MatchNode *moveTree;
+		MatchNode *matchTree;
 };
